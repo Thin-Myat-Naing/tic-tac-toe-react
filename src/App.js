df110b809e187f5b8c9872e8821f2a1c1f369d4b
Thin Myat Naing <52567884+Thin-Myat-Naing@users.noreply.github.com>
2025-05-08 17:10:59 +0700

    index.html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { StrictMode, useState } from 'react';
-import { createRoot } from 'react-dom/client';
+import { useState } from 'react';
+// import { createRoot } from 'react-dom/client';
 import './styles.css';
 
 import App from './App';
